cfbe6f961629329accec0e5db5dd2408e4461e47
Juozas Rastenis <juozas.rastenis@gmail.com>
2019-11-10 00:02:05 +0000

    logic rewritten into modular

--- src/main.rs
+++ src/main.rs
@@ -2,61 +2,21 @@ use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
-use rppal::gpio::Gpio;
-use rppal::gpio::OutputPin;
+mod stepper_28byj48;
+use stepper_28byj48::Stepper28BYJ48;
 
+mod enums;
+use enums::Direction;
+// - - - - - - - - - - - - - - - - - - - - - - - - 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
-const STEP_SEQUENCE: [[i8; 4]; 8] = [
-    [1,0,0,1],
-	[1,0,0,0],
-	[1,1,0,0],
-	[0,1,0,0],
-	[0,1,1,0],
-	[0,0,1,0],
-	[0,0,1,1],
-	[0,0,0,1]
-];
+// - - - - - - - - - - - - - - - - - - - - - - - -
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let degree: f32 = 360 as f32 * 11.377777777777;
+    let my_motor = Stepper28BYJ48::new([18,17,27,22]);
 
-    // Retrieve the GPIO pin and configure it as an output.
-    let mut _pin18: OutputPin = Gpio::new()?.get(18)?.into_output();
-    let mut _pin17: OutputPin = Gpio::new()?.get(17)?.into_output();
-    let mut _pin27: OutputPin = Gpio::new()?.get(27)?.into_output();
-    let mut _pin22: OutputPin = Gpio::new()?.get(22)?.into_output();
+    my_motor.rotate(50.0, Direction::CW);
+    thread::sleep(Duration::from_millis(200));
+    my_motor.rotate(50.0, Direction::CCW);
 
-    let mut motor_sequence_setup: [OutputPin; 4] = [
-        _pin18,
-        _pin17,
-        _pin27,
-        _pin22
-    ];
-
-    let mut row_pos: usize = 0;
-    
-    let smth: i64 = degree.round() as i64;
-
-    for _row in 0..smth {
-        for (idx, pin) in motor_sequence_setup.iter_mut().enumerate() {
-            if STEP_SEQUENCE[row_pos][idx] != 0 {
-                pin.set_high();
-            }
-            else {
-                pin.set_low();
-            }
-        }
-
-        row_pos += 1;
-    
-        if row_pos >= STEP_SEQUENCE.len() {
-            row_pos = 0
-        }
-
-        thread::sleep(Duration::from_millis(2));
-    }
-
-    Ok(for pin in motor_sequence_setup.iter_mut() {
-            pin.set_low();
-    })
+    Ok(())
 }
---
+++ src/stepper_28byj48/mod.rs
@@ -0,0 +1,91 @@
+use std::error::Error;
+use std::thread;
+use std::time::Duration;
+
+use rppal::gpio::Gpio;
+use rppal::gpio::OutputPin;
+
+use crate::enums::Direction;
+
+static STEP_SEQUENCE: [[i8; 4]; 8] = [
+    [1,0,0,1],
+    [1,0,0,0],
+    [1,1,0,0],
+    [0,1,0,0],
+    [0,1,1,0],
+    [0,0,1,0],
+    [0,0,1,1],
+    [0,0,0,1]
+];
+
+#[derive(Debug)]
+pub struct Stepper28BYJ48 {
+    pins_in_right_order: [u8; 4],
+}
+
+impl Stepper28BYJ48 {
+
+    #[inline]
+    pub(crate) fn new(pins_in_right_order: [u8; 4]) -> Stepper28BYJ48 {
+        Stepper28BYJ48 { pins_in_right_order }
+    }
+
+    #[inline]
+    pub fn motor_sequence_setup(&self) -> Result<([OutputPin; 4]), Box<dyn Error>> {
+        let p_i_o: [u8; 4] = self.pins_in_right_order;
+        let pin1: OutputPin = Gpio::new()?.get(p_i_o[0])?.into_output();
+        let pin2: OutputPin = Gpio::new()?.get(p_i_o[1])?.into_output();
+        let pin3: OutputPin = Gpio::new()?.get(p_i_o[2])?.into_output();
+        let pin4: OutputPin = Gpio::new()?.get(p_i_o[3])?.into_output();
+
+        Ok([
+            pin1,
+            pin2,
+            pin3,
+            pin4
+        ])
+    }
+
+    #[inline]
+    pub fn rotate(&self, degree: f32, direction: Direction) {
+        let steps_calculation: f32 = degree as f32 * 11.377777777777;
+
+        let mut row_pos: usize = 0;
+        
+        let steps: i64 = steps_calculation.round() as i64;
+        let mut motor_sequence_setup = self.motor_sequence_setup().unwrap();
+        
+        for _row in 0..steps {
+            for (idx, pin) in motor_sequence_setup.iter_mut().enumerate() {
+                if STEP_SEQUENCE[row_pos][idx] != 0 {
+                    pin.set_high();
+                }
+                else {
+                    pin.set_low();
+                }
+            }
+
+            match direction {
+                Direction::CW => {
+                    row_pos += 1;
+                    if row_pos >= STEP_SEQUENCE.len() {
+                        row_pos = 0
+                    }
+                }
+                Direction::CCW => {
+                    if row_pos <= 0 {
+                        row_pos = STEP_SEQUENCE.len()
+                    }
+                    row_pos -= 1;
+                }
+            }
+
+            thread::sleep(Duration::from_millis(2));
+        }
+
+        
+        for pin in motor_sequence_setup.iter_mut() {
+            pin.set_low();
+        };
+    }
+}
